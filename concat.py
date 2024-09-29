11589484781c918e4ed93052838b95dbe6134508
macdaegeon <chleorjs37@gmail.com>
2024-09-29 12:38:53 +0900

    ffmpeg 진행중

--- concat.py
+++ concat.py
@@ -4,21 +4,14 @@ import os
 
 # 인코딩 폴더(ENCODING_FOLDER) 생성
 def mkdir():
-    if(os.path.isdir('ENCODING_FOLDER') == False):
-        os.mkdir("ENCODING_FOLDER")
+    if(os.path.isdir(os.getcwd()+'/video'+'/ENCODING_FOLDER') == False):
+        os.mkdir(os.getcwd()+'/video'+'/ENCODING_FOLDER')
      
-# 코덱 확인
-def checkCodec():
-    # file_list = os.listdir('video/lauv')
-    # for i in file_list:
-    #     print(i)
-    #     video = ffmpeg.input('video/lauv/6.mp4')
-    #     print(video)
-    print(os.getcwd())
-    video = ffmpeg.input('/Users/choedaegeon/project/python-project/video/lauv/1_.mp4')
-    print(video)
-    #    video = ffmpeg.input('video/lauv/6.mp4')
-    #    print(video)
+# 코덱 변환
+def changeCodec():
+    # 리스트 불러오기
+    root = os.getcwd()+'/video'
+    ffmpeg.input(root+'/lauv'+'/1_.mp4').output(root+'/ENCODING_FOLDER'+'/1__.mp4',vcodec='libx264').run()
     
 mkdir()
-checkCodec()
+changeCodec()
